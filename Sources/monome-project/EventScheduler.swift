2a1f7af9d17d39de856e48951bbdd68a02eeb3b7
Colin McArdell <colin@colinmcardell.com>
2019-01-17 12:58:39 -0500

    Examples - Resolves issue with EventScheduler dispatch source on Linux.

---
+++ Sources/monome-project/EventScheduler.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+class EventScheduler {
+
+    enum SchedulerType {
+        case highPriority
+        case lowPriority
+        case background
+    }
+
+    typealias EventHandler = @convention(block) () -> Void
+
+    let queue: DispatchQueue
+
+    var timer: DispatchSourceTimer?
+
+    var eventHandler: EventHandler?
+
+    var interval: DispatchTimeInterval = .milliseconds(16)
+
+    init(_ type: SchedulerType = .highPriority) {
+        var dispatchQueue: DispatchQueue
+        switch type {
+        case .highPriority:
+            dispatchQueue = DispatchQueue.global(qos: .userInitiated)
+        case .lowPriority:
+            dispatchQueue = DispatchQueue.global(qos: .utility)
+        case .background:
+            dispatchQueue = DispatchQueue.global(qos: .background)
+        }
+        self.queue = dispatchQueue
+    }
+
+    deinit {
+        stop()
+    }
+
+    func start() {
+        stop()
+        timer = DispatchSource.makeTimerSource(queue: queue)
+        timer?.setEventHandler { [weak self] in
+            guard let strongSelf = self, let eventHandler = strongSelf.eventHandler else { return }
+            eventHandler()
+        }
+        timer?.schedule(deadline: .now(), repeating: interval)
+        timer?.resume()
+    }
+
+    func stop() {
+        guard let timer = timer else { return }
+        timer.setEventHandler {}
+        timer.cancel()
+        self.timer = nil
+    }
+}
--- Sources/monome-project/Monome.swift
+++ Sources/monome-project/Monome.swift
@@ -10,60 +10,6 @@ extension Monome {
     }
 }
 
-class EventScheduler {
-
-    enum SchedulerType {
-        case highPriority
-        case lowPriority
-        case background
-    }
-
-    typealias EventHandler = DispatchSourceProtocol.DispatchSourceHandler
-
-    let queue: DispatchQueue
-
-    var timer: DispatchSourceTimer?
-
-    var eventHandler: EventHandler?
-
-    var interval: DispatchTimeInterval = .milliseconds(16)
-
-    init(_ type: SchedulerType = .highPriority) {
-        var dispatchQueue: DispatchQueue
-        switch type {
-        case .highPriority:
-            dispatchQueue = DispatchQueue.global(qos: .userInitiated)
-        case .lowPriority:
-            dispatchQueue = DispatchQueue.global(qos: .utility)
-        case .background:
-            dispatchQueue = DispatchQueue.global(qos: .background)
-        }
-        self.queue = dispatchQueue
-    }
-
-    deinit {
-        stop()
-    }
-
-    func start() {
-        stop()
-        timer = DispatchSource.makeTimerSource(queue: queue)
-        timer?.setEventHandler { [weak self] in
-            guard let strongSelf = self, let eventHandler = strongSelf.eventHandler else { return }
-            eventHandler()
-        }
-        timer?.schedule(deadline: .now(), repeating: interval)
-        timer?.resume()
-    }
-
-    func stop() {
-        guard let timer = timer else { return }
-        timer.setEventHandler {}
-        timer.cancel()
-        self.timer = nil
-    }
-}
-
 class MonomeEventScheduler: EventScheduler {
 
     let monome: Monome
